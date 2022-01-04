1face8dc8eb6121cb4ee2ecdafe8a157c3083ed7
Brendan Ryan <brendantipp@gmail.com>
2022-01-04 16:23:45 +0000

    python anywhere working yaaa

---
+++ dbconfig.py
@@ -0,0 +1,9 @@
+mysql ={
+        
+        
+        'host':"brenbiz.mysql.eu.pythonanywhere-services.com",
+        'user':"brenbiz",
+        'password':"christine2009",
+
+        'database':"brenbiz$projectdb"
+}
